28c85788b7c85cc5a664411be518e22df3c4ac3b
Mikhael GERBET <frollon.noir@gmail.com>
2018-02-25 16:15:48 +0100

    feat(ep13) add components & routes
    - Add page not found component
    - Add dashboard component
    - Add routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import {DashboardModule} from './dashboard/dashboard.module';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import {AppRoutesModule} from './app.routes';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        PageNotFoundComponent
+    ],
+    imports: [
+        BrowserModule,
+        DashboardModule,
+        AppRoutesModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
---
+++ src/app/dashboard/dashboard.routes.ts
@@ -0,0 +1,13 @@
+import {DashboardComponent} from './dashboard.component';
+import {RouterModule, Routes} from '@angular/router';
+import {NgModule} from '@angular/core';
+
+export const dashboardRoutes: Routes = [
+    { path: 'dashboard', component: DashboardComponent }
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(dashboardRoutes)],
+    exports: [RouterModule]
+})
+export class DashboardRoutesRoutesModule {}
